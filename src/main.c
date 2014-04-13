a43d968f8a2b05737c3a44f518fc86f9f1ffd4cc
Lunatic Poet <lunaticzheng@gmail.com>
2014-04-13 19:01:34 +0800

    添加logo

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
 /*
- * main.c
- *
- *  Created on: 2014年4月12日
- *      Author: epay111
+ ============================================================================
+ Name        : ctest.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "epoll.h"
+#include "log.h"
+
+#define SERVER_LOGO ""\
+		"古之立大事者 不惟有超世之才 亦必有坚忍不拔之志\n"\
+		"                              ————苏轼\n"
+
+int main(void) {
+	puts(SERVER_LOGO);
+	log_debug("启动。。。。");
+
+	struct epoll *ep = epoll_open(8010);
+	epoll_loop(ep);
+	epoll_close(ep);
+	puts("END");
+	return EXIT_SUCCESS;
+}
