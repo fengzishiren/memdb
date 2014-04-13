1bb70373c90221582594427f383917701afdc7ae
Lunatic Poet <lunaticzheng@gmail.com>
2014-04-13 20:32:23 +0800

    test string_format

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@
 
 #include "epoll.h"
 #include "log.h"
+#include "string.h"
 
 #define SERVER_LOGO ""\
 		"古之立大事者 不惟有超世之才 亦必有坚忍不拔之志\n"\
@@ -20,7 +21,9 @@
 
 int main(void) {
 	puts(SERVER_LOGO);
-	log_debug("启动。。。。");
+	log_debug("启动。。。。%s", "OK");
+	struct string *s = string_format("hello %s", "World!");
+	log_error(s->value);
 
 	struct epoll *ep = epoll_open(8010);
 	epoll_loop(ep);
