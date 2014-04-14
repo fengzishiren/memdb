92e5669a4a74cf4575312781611bc8405568dd66
Lunatic Poet <lunaticzheng@gmail.com>
2014-04-15 00:43:53 +0800

    添加log

--- src/handler.c
+++ src/handler.c
@@ -21,13 +21,20 @@ void handle(struct packet *req, struct packet *resp) {
 	 */
 
 	struct command *cmd;
+	struct string *ps = string_escape(req->data);
+	log_debug("待解析数据：%s", ps->value);
+	free(ps);
 	if ((cmd = parse_to_command(req->data->value)) == NULL) {
+		log_error("协议解析出错");
 		resp->data = string_new("-ERROR 协议错误！");
 		return;
 	}
 
+	log_debug("客户端数据校验通过！");
+	log_debug("客户端数据解析成功！");
+
 	int i;
-	log_debug("argc:%d", cmd->argc);
+	log_debug("argc: %d", cmd->argc);
 	for (i = 0; i < cmd->argc; ++i) {
 		log_debug("argc: %d\t argv: %s", i, cmd->argv[i]);
 	}
